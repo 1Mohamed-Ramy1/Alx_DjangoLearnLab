e0e2850d0dde208b29aff65f8ec54486d4ee4607
ELOSTORA <mohamedramysocial@gmail.com>
2025-11-05 17:36:54 +0200

    task 1

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path, include  # <--- مهم
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls')),  # <--- ده يضيف كل روابط relationship_app
 ]
+
