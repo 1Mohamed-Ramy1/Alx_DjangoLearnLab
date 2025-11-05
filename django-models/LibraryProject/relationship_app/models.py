09a63bbce0218dda806ab254bd863fa3d0fcc82a
ELOSTORA <mohamedramysocial@gmail.com>
2025-11-05 07:52:44 +0200

    Fix queries for checker format

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -2,33 +2,15 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
-    nationality = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
-
-    def __str__(self):
-        return self.name
-
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
