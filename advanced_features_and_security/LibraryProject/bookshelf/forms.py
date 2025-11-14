f66dd55bcbd84527a0f5894900cad576cd3d669e
ELOSTORA <mohamedramysocial@gmail.com>
2025-11-14 20:37:04 +0200

    Task2

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,15 +1,7 @@
 from django import forms
+from .models import Book
 
-
-class SearchForm(forms.Form):
-    """
-    SearchForm for validating and sanitizing user input.
-    
-    Prevents SQL Injection and XSS attacks by using Django Forms
-    which automatically cleans and validates data.
-    """
-    query = forms.CharField(
-        max_length=255,
-        required=False,
-        widget=forms.TextInput(attrs={'placeholder': 'Search books...'})
-    )
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
