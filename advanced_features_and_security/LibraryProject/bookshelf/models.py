50d60a8ec2f3525883b8322588e888e928e44f90
ELOSTORA <mohamedramysocial@gmail.com>
2025-11-14 19:51:52 +0200

    fix

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,14 +1,3 @@
 from django.db import models
-from django.apps import apps
-from django.conf import settings
 
-# Reference the actual custom user model so static tools can resolve it:
-CustomUser = apps.get_model(settings.AUTH_USER_MODEL)
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
-
-    def __str__(self):
-        return f"{self.title} by {self.author}"
+# Only the Book model should be here. CustomUser is defined in users/models.py
