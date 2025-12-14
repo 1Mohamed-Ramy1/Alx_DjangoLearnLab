a72d77cf493fb7a29d91e381a82454dfba2d8bca
ELOSTORA <mohamedramysocial@gmail.com>
2025-12-14 23:38:28 +0200

    Implementing User Follows and Feed Functionality

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import RegisterView, LoginView, ProfileView
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet
+from .views import FollowUserView, UnfollowUserView
 
 router = DefaultRouter()
 router.register('users', UserViewSet)
@@ -12,4 +13,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path('profile/', ProfileView.as_view()),
+    path('follow/<int:pk>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:pk>/', UnfollowUserView.as_view(), name='unfollow-user'),
 ]
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,57 +1,27 @@
-from rest_framework.views import APIView
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.authtoken.models import Token
-from .serializers import RegisterSerializer, LoginSerializer, ProfileSerializer
-from rest_framework.decorators import action
-from rest_framework import status, viewsets
-from rest_framework.permissions import IsAuthenticated
-from .models import User
+from .models import User as CustomUser
 from .serializers import FollowSerializer
-class RegisterView(APIView):
-    permission_classes = [AllowAny]
 
-    def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        token, _ = Token.objects.get_or_create(user=user)
-        return Response({
-            'token': token.key
-        })
-
-class LoginView(APIView):
-    permission_classes = [AllowAny]
-
-    def post(self, request):
-        serializer = LoginSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data
-        token, _ = Token.objects.get_or_create(user=user)
-        return Response({
-            'token': token.key
-        })
-
-class ProfileView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        serializer = ProfileSerializer(request.user)
-        return Response(serializer.data)
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+# Follow a user
+class FollowUserView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
     serializer_class = FollowSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
-    @action(detail=True, methods=['post'])
-    def follow(self, request, pk=None):
+    def post(self, request, pk, *args, **kwargs):
         user_to_follow = self.get_object()
         request.user.following_users.add(user_to_follow)
         return Response({"detail": f"You are now following {user_to_follow.username}"}, status=status.HTTP_200_OK)
 
-    @action(detail=True, methods=['post'])
-    def unfollow(self, request, pk=None):
+
+# Unfollow a user
+class UnfollowUserView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = FollowSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, pk, *args, **kwargs):
         user_to_unfollow = self.get_object()
         request.user.following_users.remove(user_to_unfollow)
         return Response({"detail": f"You have unfollowed {user_to_unfollow.username}"}, status=status.HTTP_200_OK)
